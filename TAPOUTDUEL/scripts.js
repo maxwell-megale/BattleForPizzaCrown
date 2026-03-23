1d4cbf02792c35f07192b43bcde0e878220b291f
Max <megale.maxwellm@gmail.com>
2026-03-23 14:21:18 -0500

    fixed timer repeat error

--- TAPOUTDUEL/scripts.js
+++ TAPOUTDUEL/scripts.js
@@ -5,34 +5,36 @@ var started = false;
 var timerMin = 1;
 var seconds = 60;
 var mins = timerMin;
+var timeoutId = null;
 
-	
+
 
 
 function countdown(minutes) {
-    seconds = 60;
-    mins = minutes
-    function tick() {
-    	if(timerActive){
-        //This script expects an element with an ID = "counter". You can change that to what ever you want. 
-        var current_minutes = mins-1
-        seconds--;
-        display.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-        console.log(timerActive);
-        if( seconds > 0) {
-            setTimeout(tick, 1000);
-        } else if(mins > 1){
-                countdown(mins-1);           
-        }else {
-        	timerActive = false;
-        }  
-    }
-}
-tick();
+	clearTimeout(timeoutId);
+	seconds = 60;
+	mins = minutes
+	function tick() {
+		if (timerActive) {
+			//This script expects an element with an ID = "counter". You can change that to what ever you want. 
+			var current_minutes = mins - 1
+			seconds--;
+			display.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+			console.log(timerActive);
+			if (seconds > 0) {
+				timeoutId = setTimeout(tick, 1000);
+			} else if (mins > 1) {
+				countdown(mins - 1);
+			} else {
+				timerActive = false;
+			}
+		}
+	}
+	tick();
 
 }
 
-function tapOut(){ //Hide everything on screen and say TAPPED OUT
+function tapOut() { //Hide everything on screen and say TAPPED OUT
 	document.getElementById("p1Score").style.display = "none";
 	document.getElementById("p1").style.display = "none";
 	document.getElementById("tapLogo").style.display = "none";
@@ -41,46 +43,47 @@ function tapOut(){ //Hide everything on screen and say TAPPED OUT
 	document.getElementById("tappedOut").style.display = "inline";
 }
 
-function nextTimer(){
+function nextTimer() {
 	timerActive = true;
 	score++;
 	countdown(timerMin);
 	document.getElementById("p1Score").innerHTML = score;
 }
 
-function startTimer(){
+function startTimer() {
 	started = true;
-	timerActive	= true;
+	timerActive = true;
 	console.log("start");
-    display = document.getElementById("counter");
-    display.innerHTML = "01"+ ":" + "00";
+	display = document.getElementById("counter");
+	display.innerHTML = "01" + ":" + "00";
 	countdown(timerMin);
 }
 
-function skipTimer(){
+function skipTimer() {
 	timerActive = false;
+	clearTimeout(timeoutId);
 	timer = timerMin;
 	seconds = 60;
 	mins = timerMin;
-	display.innerHTML = "01"+ ":" + "00";
+	display.innerHTML = "01" + ":" + "00";
 }
 
 document.body.addEventListener("keyup", (ev) => {
-		if(ev.key == "5"){ //Start counter button
-			if(!started){
-				startTimer();
-			}else if (!timerActive) {
-				nextTimer();
-			}
+	if (ev.key == "5") { //Start counter button
+		if (!started) {
+			startTimer();
+		} else if (!timerActive) {
+			nextTimer();
 		}
+	}
 
-		if(ev.key == "4"){ //skip counter button
-			if(started && timerActive){
-				skipTimer();
-			}
+	if (ev.key == "4") { //skip counter button
+		if (started && timerActive) {
+			skipTimer();
 		}
+	}
 
-		if(ev.key == "2") { // Tap Out button
-			tapOut();
-		}
+	if (ev.key == "2") { // Tap Out button
+		tapOut();
+	}
 });
